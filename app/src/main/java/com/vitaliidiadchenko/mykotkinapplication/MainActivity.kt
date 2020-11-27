34c978760207a04e8d0d71bef6e9d84bdd34555b
VitaliiDiadchenko <vitaly.dyadchenko@gmail.com>
2020-11-27 22:51:50 +0300

    Add FragmentMoviesDetails.kt Add FragmentMovieList.kt Add interface FragmentListener.kt Add fragment_movies_list.xml Fix ageRatingBackground in movieDetailScreen

--- app/src/main/java/com/vitaliidiadchenko/mykotkinapplication/MainActivity.kt
+++ app/src/main/java/com/vitaliidiadchenko/mykotkinapplication/MainActivity.kt
@@ -3,15 +3,26 @@ package com.vitaliidiadchenko.mykotkinapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        R.layout.activity_main
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, FragmentMoviesList()).commit()
+        }
     }
 
-    private fun moveToMovieDetail() {
-        val intent = Intent(this, MovieDetailActivity::class.java)
-        startActivity(intent)
+    override fun goToFragmentMoviesDetails() {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.main_container, FragmentMoviesDetails())
+            .commit()
+    }
+
+    override fun goToFragmentMoviesList() {
+        supportFragmentManager.popBackStack()
     }
 }
